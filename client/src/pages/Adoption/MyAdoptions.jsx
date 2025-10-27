6dcbe879bccc8aa3c97fbe4336443552f2554d93
Methni0616 <methnimanarandi7@gmail.com>
2025-10-27 13:23:58 +0530

    update

---
+++ client/src/pages/Adoption/MyAdoptions.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function MyAdoptions() {
+  return (
+    <div>
+      <h1>My Adoptions</h1>
+      <p>Track your adoption applications (pending, approved, completed).</p>
+    </div>
+  );
+}
