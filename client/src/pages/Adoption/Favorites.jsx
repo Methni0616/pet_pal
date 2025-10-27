4d1c8416784f7e59f03dcad1dec281138848292f
Methni0616 <methnimanarandi7@gmail.com>
2025-10-27 13:23:52 +0530

    update

---
+++ client/src/pages/Adoption/Favorites.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Favorites() {
+  return (
+    <div>
+      <h1>Favorites</h1>
+      <p>Your saved pets will appear here.</p>
+    </div>
+  );
+}
