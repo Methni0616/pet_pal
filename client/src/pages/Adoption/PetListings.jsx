8aebcd4db8b785bdd6f4a6c2935a0c97b53ad72e
Methni0616 <methnimanarandi7@gmail.com>
2025-11-02 14:24:13 +0530

    update

--- client/src/pages/Adoption/PetListings.jsx
+++ client/src/pages/Adoption/PetListings.jsx
@@ -18,7 +18,7 @@ export default function PetListings() {
     { id: 10, name: "Oliver", species: "Cat", breed: "Bengal", age: "2 years", image: "https://tse1.mm.bing.net/th/id/OIP.zq-Fem02l2uLcQBgmel2kwHaFO?rs=1&pid=ImgDetMain&o=7&rm=3" },
     { id: 11, name: "Buddy", species: "Dog", breed: "Labrador Retriever", age: "3.5 years", image: "https://www.itl.cat/pngfile/big/111-1118405_labrador-retriever-dog-hd-wallpapers-labrador-dog.jpg" },
     { id: 12, name: "Chirpy", species: "Bird", breed: "Parakeet", age: "10 months", image: "https://tse1.mm.bing.net/th/id/OIP.Gn607i1fZf7Q2fORzs0sQAHaEy?rs=1&pid=ImgDetMain&o=7&rm=3" },
-    { id: 13, name: "Hazel", species: "Rabbit", breed: "Lionhead", age: "1.2 years", image: "https://images.unsplash.com/photo-1574158622682-e40e69881006" },
+    { id: 13, name: "Hazel", species: "Rabbit", breed: "Lionhead", age: "1.2 years", image: "https://th.bing.com/th/id/R.f7884c7a1fc231a18c989696be586a2e?rik=zLNMqKTE9f8HdQ&pid=ImgRaw&r=0" },
     { id: 14, name: "Pumpkin", species: "Cat", breed: "Persian", age: "4 years", image: "https://images.unsplash.com/photo-1574158622682-e0a2949d1b3c" },
     { id: 15, name: "Ziggy", species: "Dog", breed: "Husky", age: "2.8 years", image: "https://images.unsplash.com/photo-1555685812-4b943f1cb0eb" },
     { id: 16, name: "Peanut", species: "Hamster", breed: "Dwarf", age: "5 months", image: "https://images.unsplash.com/photo-1611095973511-9f3a07f4a858" },
