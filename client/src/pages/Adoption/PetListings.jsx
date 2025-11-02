cabcf0824ba66fbd94c4950ab47f98e8696b7512
Methni0616 <methnimanarandi7@gmail.com>
2025-11-02 14:27:04 +0530

    update

--- client/src/pages/Adoption/PetListings.jsx
+++ client/src/pages/Adoption/PetListings.jsx
@@ -21,7 +21,7 @@ export default function PetListings() {
     { id: 13, name: "Hazel", species: "Rabbit", breed: "Lionhead", age: "1.2 years", image: "https://th.bing.com/th/id/R.f7884c7a1fc231a18c989696be586a2e?rik=zLNMqKTE9f8HdQ&pid=ImgRaw&r=0" },
     { id: 14, name: "Pumpkin", species: "Cat", breed: "Persian", age: "4 years", image: "https://fishsubsidy.org/wp-content/uploads/2020/02/persian-cat1.jpg" },
     { id: 15, name: "Ziggy", species: "Dog", breed: "Husky", age: "2.8 years", image: "https://tse3.mm.bing.net/th/id/OIP.IitgyH10SCQXv6SA2slRGQHaEo?rs=1&pid=ImgDetMain&o=7&rm=3" },
-    { id: 16, name: "Peanut", species: "Hamster", breed: "Dwarf", age: "5 months", image: "https://images.unsplash.com/photo-1611095973511-9f3a07f4a858" },
+    { id: 16, name: "Peanut", species: "Hamster", breed: "Dwarf", age: "5 months", image: "https://tse4.mm.bing.net/th/id/OIP.iw-pj4A5EcWYR-uLryiPIgHaFS?rs=1&pid=ImgDetMain&o=7&rm=3" },
     { id: 17, name: "Kiwi", species: "Bird", breed: "Lovebird", age: "7 months", image: "https://images.unsplash.com/photo-1616511121803-992a77b90c39" },
     { id: 18, name: "Toby", species: "Dog", breed: "Bulldog", age: "4.5 years", image: "https://images.unsplash.com/photo-1558788353-5e7e9cbe4c8e" },
     { id: 19, name: "Misty", species: "Cat", breed: "Ragdoll", age: "2 years", image: "https://images.unsplash.com/photo-1618826411640-7de6d1b6b36f" },
