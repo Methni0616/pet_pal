f6c3c672d3cbb991df9e5d65ce07381eb730ab2f
Methni0616 <methnimanarandi7@gmail.com>
2025-11-07 17:21:40 +0530

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,13 +5,20 @@ import Home from "./pages/Home/Home";
 import Login from "./pages/Auth/Login";
 import Register from "./pages/Auth/Register";
 
-
 import PetListings from "./pages/Adoption/PetListings";
 import PetDetails from "./pages/Adoption/PetDetails";
 import Favorites from "./pages/Adoption/Favorites";
 import AdoptionForm from "./pages/Adoption/AdoptionForm";
 import Shelter from "./pages/Adoption/Shelter";
 import MyAdoptions from "./pages/Adoption/MyAdoptions";
+
+import MyPets from "./pages/care/MyPets";
+import AddPet from "./pages/care/AddPet";
+import PetProfile from "./pages/care/PetProfile";
+import HealthRecords from "./pages/care/HealthRecords";
+import Reminders from "./pages/care/Reminders";
+import ActivityTracker from "./pages/care/ActivityTracker";
+import PetGallery from "./pages/care/PetGallery";
 
 function App() {
   return (
@@ -20,12 +27,21 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+
         <Route path="/adopt" element={<PetListings />} />
         <Route path="/adopt/:id" element={<PetDetails />} />
         <Route path="/favorites" element={<Favorites />} />
         <Route path="/adoption-form/:id" element={<AdoptionForm />} />
         <Route path="/shelter/:id" element={<Shelter />} />
         <Route path="/my-adoptions" element={<MyAdoptions />} />
+
+        <Route path="/care" element={<MyPets />} />
+        <Route path="/care/add" element={<AddPet />} />
+        <Route path="/care/profile/:id" element={<PetProfile />} />
+        <Route path="/care/health" element={<HealthRecords />} />
+        <Route path="/care/reminders" element={<Reminders />} />
+        <Route path="/care/activity" element={<ActivityTracker />} />
+        <Route path="/care/gallery" element={<PetGallery />} />
       </Routes>
     </Router>
   );
