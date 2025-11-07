124c66f59655a7e698eefac028670a6ba74c5e87
Methni0616 <methnimanarandi7@gmail.com>
2025-11-07 17:21:55 +0530

    update

---
+++ client/src/pages/care/ActivityTracker.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function ActivityTracker() {
+  return (
+    <div style={{ padding: "2rem", textAlign: "center" }}>
+      <h1>Activity Tracker</h1>
+      <p>This page will show your pet’s daily activities soon!</p>
+    </div>
+  );
+}
