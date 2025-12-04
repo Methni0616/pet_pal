2ba361e198069a1534792fe747b6f6545d756e06
Methni0616 <methnimanarandi7@gmail.com>
2025-12-04 21:27:24 +0530

    update

--- client/src/pages/Adoption/Favorites.jsx
+++ client/src/pages/Adoption/Favorites.jsx
@@ -4,7 +4,7 @@ import "./Favorites.css";
 export default function Favorites() {
   const [favorites, setFavorites] = useState([]);
 
-  // Load favoritefrm loalStorage
+  // Load favorites from localStorage
   useEffect(() => {
     const saved = JSON.parse(localStorage.getItem("favorites")) || [];
     setFavorites(saved);
