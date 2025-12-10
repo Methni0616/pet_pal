93420c7e9178889baddb259544e5b80afc4f88a0
Methni0616 <methnimanarandi7@gmail.com>
2025-12-10 21:41:23 +0530

    update

--- client/src/pages/Adoption/Favorites.jsx
+++ client/src/pages/Adoption/Favorites.jsx
@@ -10,7 +10,7 @@ export default function Favorites() {
     setFavorites(saved);
   }, []);
 
-  // Rmove petom oes
+  // Rmove petom os
   const removeFavorite = (id) => {
     const updated = favorites.filter((pet) => pet.id !== id);
     setFavorites(updated);
